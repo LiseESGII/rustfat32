56646f00793a8c37d993c0830b4ca4557305129a
LiseESGII <lisebpub@outlook.fr>
2025-05-28 11:31:22 +0200

    [ADD] : Boot minimal et préparation

---
+++ target/x86_64-unknown-none/release/deps/fat32_kernel-223ec95cb9447080.d
@@ -0,0 +1,5 @@
+/home/lise/rustfat32/fat32_kernel/target/x86_64-unknown-none/release/deps/fat32_kernel-223ec95cb9447080: src/main.rs
+
+/home/lise/rustfat32/fat32_kernel/target/x86_64-unknown-none/release/deps/fat32_kernel-223ec95cb9447080.d: src/main.rs
+
+src/main.rs:
---
+++ target/x86_64-unknown-none/release/fat32_kernel.d
@@ -0,0 +1,1 @@
+/home/lise/rustfat32/fat32_kernel/target/x86_64-unknown-none/release/fat32_kernel: /home/lise/rustfat32/fat32_kernel/src/main.rs
