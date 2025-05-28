8471f8d6f11598f7cb1e4a67559b852f818f83f9
LiseESGII <lisebpub@outlook.fr>
2025-05-28 13:25:53 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 #![no_std]      // Désactive la stdlib
 #![no_main]     // Désactive le point d’entrée Rust standard
 
+mod boot_sector;
+
+
 use core::panic::PanicInfo;
 
 /// Handler appelé en cas de panic
@@ -12,5 +15,7 @@ fn panic(_info: &PanicInfo) -> ! {
 /// Point d’entrée appelé par l’assembleur
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    // Appel du test manuel du Boot Sector
+    boot_sector::test_boot_sector_parsing();
     loop {}
 }
